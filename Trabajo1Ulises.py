0623a9f2685830314f6d55e09143554183ba63be
Ulises <gossuinulises@gmail.com>
2023-08-16 18:28:38 -0300

    Primer Trabajo

---
+++ Trabajo1Ulises.py
@@ -0,0 +1,59 @@
+
+#1#
+numero1 = 5
+
+#2#
+numero2 = 5.5
+
+#3#
+suma = numero1 + numero2
+print ("La suma de estos números es: ", suma )
+
+#4#
+resta = numero1 - numero2
+multi = numero1 * numero2
+divis = numero1 / numero2
+print ("La resta de estos números es: ", resta)
+print ("La multiplicación de estos números es: ", multi )
+print ("La división de estos números es: ", divis)
+
+#5#
+nombre = "Ulises"
+
+#6#
+precio = 50.99
+
+#7#
+descuento = 0.25
+
+#8#
+precio_final = precio - precio * descuento
+
+#9#
+cadena = "Lorem ipsum"
+
+#10#
+longitud = len(cadena)
+
+#11#
+precio = int(80.50)
+
+#12#
+nombre = "Ulises"
+apellido = "Gossuin"
+nombre_completo = nombre + " " + apellido
+
+#13#
+edad = 19
+edad += 5
+edad -= 10
+
+#14#
+altura = (1.76 * 4) / 3
+
+#15#
+nombre_mayus = "ULISES"
+nombre_minus = nombre_mayus.lower()
+
+#16#
+nombre_title = nombre_mayus.title()
