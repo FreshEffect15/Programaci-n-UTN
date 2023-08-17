4025c1199f9340da11d35c25f56fbb760453ddbd
FreshEffect15 <142315233+FreshEffect15@users.noreply.github.com>
2023-08-17 09:58:53 -0300

    Add files via upload

---
+++ TpPT2Gossuin.py
@@ -0,0 +1,51 @@
+#Ejercicio 1 
+
+base = int(input("Ingrese la base del rectángulo "))
+
+altura = int(input("Ingrese la altura del rectángulo "))
+
+area = base * altura
+
+perimetro = 2 * altura + 2 * base
+
+print("El area del rectángulo es: ", area)
+
+print("El perímetro del rectángulo es: ", perimetro)
+
+#Ejercicio 2
+
+cateto1 = int(input("Ingrese el primer cateto del triángulo"))
+
+cateto2 = int(input("Ingrese el segundo cateto del triángulo"))
+
+hipotenusa = cateto1**2 + cateto2**2
+
+print("La hipotenusa es: ", hipotenusa)
+
+#Ejercicio 3
+
+num1 = int(input("Ingrese el primer número"))
+
+num2 = int(input("Ingrese el segundo número"))
+
+suma = num1 + num2
+
+resta = num1 - num2
+
+multi = num1 * num2
+
+divis = num1 / num2
+
+print("El resultado de la suma de los números es: ", suma)
+
+print("El resultado de la resta de los números es: ", resta)
+
+print("El resultado de la multiplicación de los números es: ", multi)
+
+print("El resultado de la división de los números es: ", divis)
+
+#Ejercicio 4
+
+farenheit = int(input("Ingrese una cantidad de grados farenheit"))
+
+celsius = (farenheit - )
